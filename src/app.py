9afc51b08ab11ba8026704341d0e6776d2017050
coleutk <colepricecollege@gmail.com>
2025-03-04 19:14:45 -0500

    Reworked University Filtering (US) + Fuzzy Finding to Match + Updated DB

--- src/app.py
+++ src/app.py
@@ -15,34 +15,21 @@ def home():
         school_name = request.form.get('school_input')
         return redirect(url_for('school', school_name=school_name))
     
-    # Default display
-    school_ratings = database.grab_all_school_ratings();
-
+    # Default display (GET)
+    # Display averages here
     result = []
-    for school, ratings in school_ratings.items():
-        avg = round(sum(ratings) / len(ratings), 2)
-        result.append((avg, school))
-
-    result.sort(reverse=True)
-
+    result = database.calculate_university_averages()
     return render_template('index.html', result=result)
 
 
 
 @app.route('/school', methods=['GET', 'POST'])
 def school():
-    # This is for if we're already on the /school page and a user types in search bar
     if request.method == 'POST':
         school_name = request.form.get('school_input')
         users = database.get_users_by_school(school_name)
-        users.sort(key=lambda user: user['contestRating'], reverse=True)
+        users.sort(key=lambda user: user['currentRating'], reverse=True)
         return render_template('school.html', users=users, school_name=school_name)
-    
-    # This is for if we just got redirected from /home
-    school_name = request.args.get('school_name', '') # Get school name from url
-    users = database.get_users_by_school(school_name)
-    users.sort(key=lambda user: user['contestRating'], reverse=True)
-    return render_template('school.html', users=users, school_name=school_name)
 
 
 if __name__ == '__main__':
--- src/database.py
+++ src/database.py
@@ -1,10 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-from typing import List, Set, Dict
+from typing import List, Dict, Tuple
 
 from pymongo import MongoClient
-from rapidfuzz import process, fuzz
+from rapidfuzz.process import extractOne
+from rapidfuzz import fuzz
+# from rapidfuzz.distance import Levenshtein
+
+from data.college_aliases import university_aliases
 
 
 #load env vars
@@ -12,28 +16,13 @@ load_dotenv()
 mongodb_uri = os.getenv("MONGODB_URI")
 
 client = MongoClient(mongodb_uri)
+# Grabs collection from db
 leet_users = client.leeterboard.leet_users
-
 
 def _serialize_users(users: List[dict]) -> List[dict]:
     for user in users:
         user['_id'] = str(user['_id'])
     return users
-
-
-def upsert_user(user: dict) -> None:
-    user['username'] = user['username'].lower()
-    user['contestRating'] = float(user['contestRating'])
-    leet_users.update_one(
-        {'username': user['username']},
-        {'$set': {
-            'contestRating': user['contestRating'],
-            'username': user['username'],
-            'userAvatar': user['userAvatar'],
-            'school': user['school'],
-        }},
-        upsert=True,
-    )
 
 
 def get_users_by_school(school: str) -> List[dict]:
@@ -46,57 +35,96 @@ def get_users_by_school(school: str) -> List[dict]:
 
 
 
+# Given an document in the db collection, update the school in the entry to the new value
+def upsert_user(user: dict, matched_school: str) -> None:
+    leet_users.update_one(
+        {'username': user['username']},
+        {'$set': {
+            'school': matched_school
+        }},
+        upsert=True,
+    )
 
 
-# University list for matching to original [lower matching name, original name]
-school_dict = {}
+# Opens file and stores university names in a map, as well as a list for fuzzy finding
+school_names = {}
+def open_file():
+    with open('src/data/all-colleges.txt') as file:
+        for line in file:
+            school_name = line.rsplit(" (", 1)[0]
+            school_name_lower = school_name.lower()
+            
+            school_names[school_name_lower] = school_name
 
-# Open file of all colleges
-with open('src/data/all-colleges.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        original_name = line.rsplit(" (", 1)[0]
-        matching_name = original_name.lower()
-        school_dict[matching_name] = original_name
+open_file()
+lowercase_university_names = list(school_names.keys())
 
-# Lowercase list for fuzzy matching
-school_list_lower = list(school_dict.keys())
 
+# search all-colleges.txt for fuzzy matches
 def standardize_school_name(school: str) -> str:
-    school_input = school.lower().strip()
-    # This calculates a similarity score that assigns match to the best match in 'school_list_lower'
-    match, score, _ = process.extractOne(school_input, school_list_lower, scorer=fuzz.ratio)
-    
+    # See if the user input was an alias first
+    if school in university_aliases:
+        school = university_aliases[school]
+
+    # best match in lowercase_university_names, score, index (which we don't need)
+    match, score, _ = extractOne(school, lowercase_university_names, scorer=fuzz.token_set_ratio)
+
+    # If a match score of 80 is determined, add university name from list to database
     if score > 80:
-        return school_dict[match]
+        return school_names[match]
     else:
         return None
 
 
+# Grabs all schools from db and updates their info
+def standardize_db_universities():
+    # Grabs document from leet_users collection
+    cursor = leet_users.find()
 
-# Grabs all schools' contest rating averages
-def grab_all_school_ratings() -> Dict[str, List[float]]:
-    cursor = leet_users.find(
-        {
-            'school': {
-                '$ne': None,
-                '$regex': 'University'
-            }
-        },
-        {'school': 1, 'contestRating': 1, '_id': 0}
-    )
+    # Iterating through each document in the collection
+    for user in cursor:
+        # Grab original school name in db
+        original_school = user['school']
 
-    school_ratings = {}
-    for entry in cursor:
-        original_school = entry['school']
+        # Try to match it with a standard one from list
         standardized_school = standardize_school_name(original_school)
 
-        if standardized_school is None: continue
+        if standardized_school is not None: # Match found
+            # Replace the user's original school with the updated name in the db
+            upsert_user(user, standardized_school)
+        else: # No match found
+            # Remove user from db
+            leet_users.delete_one({'username': user['username']})
+            updated_doc = leet_users.find_one({'username': user['username']})
 
-        rating = entry['contestRating']
+            if updated_doc:
+                # Making sure its actually deleting the doc
+                print('doc not deleted')
 
-        if standardized_school not in school_ratings:
-            school_ratings[standardized_school] = []
 
-        school_ratings[standardized_school].append(rating)
 
-    return school_ratings
+
+# API FUNCTIONS FOR YOU TO CALL IN APP.PY
+
+
+# Returns a list of tuples (university name, avg contest rating)
+def calculate_university_averages() -> List[Tuple[float, str]]:
+    all_school_ratings = {}
+    cursor = leet_users.find()
+
+    for user in cursor:
+        school = user['school']
+        current_rating = user['currentRating']
+
+        if school not in all_school_ratings:
+            all_school_ratings[school] = []
+
+        all_school_ratings[school].append(current_rating)
+
+    avg_school_ratings = []
+    for school, ratings in all_school_ratings.items():
+        avg = round(sum(ratings) / len(ratings), 2)
+        avg_school_ratings.append((avg, school))
+
+    avg_school_ratings.sort(reverse=True)
+    return avg_school_ratings
