8a1e513f4d878905ab861c11c3da636272f51d2d
coleutk <colepricecollege@gmail.com>
2025-04-09 13:04:08 -0400

    Started writing script to calculate max page + fixed /unipage2 foundation

--- src/scripts/grab_weekly_contest.py
+++ src/scripts/grab_weekly_contest.py
@@ -1,64 +1,100 @@
 import requests
-import json
 import asyncio
 import aiohttp
+import json
+import time
 
-# Simply returns the proper url with the modified portions
-def get_weekly_contest_url(contest_id: int, page: int) -> str:
-    return f"https://leetcode.com/contest/api/ranking/weekly-contest-{contest_id}/?pagination={page}&region=global_v2"
+# Binary search to find max page size
+def find_max_page(contest_id: int):
+    low = 1
+    high = 5000
+
+    while(low <= high):
+        mid = low + (high - low) // 2
+        print(mid)
+
+        # Gives us permission to make the request (Avoid 403)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36",
+            "Referer": "https://leetcode.com/contest/weekly-contest-437/ranking/1229/?region=global_v2",
+            "Content-Type": "application/json"
+        }
+
+        # Slight delay to avoid rate limiting
+        time.sleep(1)
+
+        response = requests.get(
+            f"https://leetcode.com/contest/api/ranking/weekly-contest-{contest_id}/?pagination={mid}&region=global_v2",
+            headers=headers
+        )
+
+        # Even if the page is too large, it should still return a 200, but with empty data
+        if response.status_code == 200:
+            print(f"Status code: {response.status_code}")
+            # Logic here
+
+        break
+
+find_max_page(440)
 
 
-# Makes the individual HTTP request and waits for the response body and parses it as JSON
-async def fetch_contest_page(session, url, headers):
-    async with session.get(url, headers=headers, ssl=False) as response:
-        return await response.json()
 
 
-# Performs all of the asynchronous requests
-async def request_all_contest_pages(batch_size: int):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36",
-        "Referer": "https://leetcode.com/contest/weekly-contest-437/ranking/1229/?region=global_v2",
-        "x-csrftoken": "8bg8dnJkupMC9wgGkgkuXkpoND0D4pooy8qrEWc93k4O64npnl4JJ6mioYgoX1JK",
-        "Content-Type": "application/json"
-    }
+# # Simply returns the proper url with the modified portions
+# def get_weekly_contest_url(contest_id: int, page: int) -> str:
+#     return f"https://leetcode.com/contest/api/ranking/weekly-contest-{contest_id}/?pagination={page}&region=global_v2"
 
-    # Generate all URLs (1 per page)
-    all_urls = []
-    contest_number = 440
-    contest_max_pages = 1264
-    for page_number in range(1, contest_max_pages):
-        all_urls.append(get_weekly_contest_url(contest_number, page_number))
 
-    responses = []
-    async with aiohttp.ClientSession() as session:
-        # Process requests in batches
-        # 0 -> 9, 10 -> 19, 20 -> 29k
-        for i in range(0, len(all_urls), batch_size):
-            print(f"Making request {i}")
-            batch = all_urls[i:i + batch_size]
+# # Makes the individual HTTP request and waits for the response body and parses it as JSON
+# async def fetch_contest_page(session, url, headers):
+#     async with session.get(url, headers=headers, ssl=False) as response:
+#         return await response.json()
+
+
+# # Performs all of the asynchronous requests
+# async def request_all_contest_pages(batch_size: int):
+#     headers = {
+#         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36",
+#         "Referer": "https://leetcode.com/contest/weekly-contest-437/ranking/1229/?region=global_v2",
+#         "Content-Type": "application/json"
+#     }
+
+#     # Generate all URLs (1 per page)
+#     all_urls = []
+#     contest_number = 440
+#     contest_max_pages = 1264
+#     for page_number in range(1, contest_max_pages):
+#         all_urls.append(get_weekly_contest_url(contest_number, page_number))
+
+#     responses = []
+#     async with aiohttp.ClientSession() as session:
+#         # Process requests in batches
+#         # 0 -> 9, 10 -> 19, 20 -> 29k
+#         for i in range(0, len(all_urls), batch_size):
+#             print(f"Making request {i}")
+#             batch = all_urls[i:i + batch_size]
             
-            # Go through the batch
-            tasks = []
-            for batch_url in batch:
-                tasks.append(fetch_contest_page(session, batch_url, headers))
+#             # Go through the batch
+#             tasks = []
+#             for batch_url in batch:
+#                 tasks.append(fetch_contest_page(session, batch_url, headers))
             
-            # Send out all batched requests and store responses
-            batch_responses = await asyncio.gather(*tasks)
-            responses.extend(batch_responses)
+#             # Send out all batched requests and store responses
+#             batch_responses = await asyncio.gather(*tasks)
+#             responses.extend(batch_responses)
 
-            if i + batch_size < len(all_urls):
-                await asyncio.sleep(1)
+#             if i + batch_size < len(all_urls):
+#                 await asyncio.sleep(1)
 
-    return responses
+#     return responses
 
-results = asyncio.run(request_all_contest_pages(10))
+# results = asyncio.run(request_all_contest_pages(10))
 
-all_new_users = []
-for data in results:
-    for user in data['total_rank']:
-        if(user['country_code'] == 'US'):
-            all_new_users.append(user)
+# all_new_users = []
+# for data in results:
+#     for user in data['total_rank']:
+#         if(user['country_code'] == 'US'):
+#             all_new_users.append(user)
     
 
 # Now, we want to see how many of these users have a school
